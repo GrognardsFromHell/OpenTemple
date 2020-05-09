95765e9c89a1465bc2ae4dfb14b388f9a98ef34e
Sebastian Hartte <sebastian@hartte.de>
2020-05-09 13:32:04 +0200

    More Stuff

---
+++ Data/ui/ui.ts
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE" sourcelanguage="en_US">
+<context>
+    <name>MainMenu</name>
+    <message>
+        <location filename="mainmenu/MainMenu.qml" line="32"/>
+        <source>New Game</source>
+        <comment>#{main_menu:0</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainmenu/MainMenu.qml" line="35"/>
+        <source>Load Game</source>
+        <comment>#{main_menu:1</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainmenu/MainMenu.qml" line="38"/>
+        <source>Tutorial</source>
+        <comment>#{main_menu:2</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainmenu/MainMenu.qml" line="41"/>
+        <source>Options</source>
+        <comment>#{main_menu:3</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainmenu/MainMenu.qml" line="44"/>
+        <source>Quit Game</source>
+        <comment>#{main_menu:4</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainmenu/MainMenu.qml" line="52"/>
+        <location filename="mainmenu/MainMenu.qml" line="61"/>
+        <source>Exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
