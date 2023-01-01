00801d0a85eb1bf4907339d4f773b53404ddc006
Sebastian Hartte <shartte@users.noreply.github.com>
2023-01-01 18:01:10 +0100

    Add a small crosshair to the debug overlay.

--- Core/DebugUI/DebugOverlay.cs
+++ Core/DebugUI/DebugOverlay.cs
@@ -1,8 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Numerics;
 using ImGuiNET;
 using OpenTemple.Core.GameObjects;
+using OpenTemple.Core.GFX;
 using OpenTemple.Core.Systems;
+using OpenTemple.Core.TigSubsystems;
 using OpenTemple.Core.Ui;
 
 namespace OpenTemple.Core.DebugUI;
@@ -13,13 +16,36 @@ public static class DebugOverlay
 
     public static void Render(IGameViewport viewport)
     {
-        if (ImGui.Begin( "Debug Overlay", ref _isActive))
+        if (ImGui.Begin("Debug Overlay", ref _isActive))
         {
             var mousePt = Globals.UiManager.MousePos;
+            var gameViewRect = ((GameView) viewport).GetViewportBorderArea();
+            mousePt.X -= gameViewRect.X;
+            mousePt.Y -= gameViewRect.Y;
 
-            var worldCoord = viewport.ScreenToTile(mousePt.X, mousePt.Y);
-            ImGui.Text($"{worldCoord}");
+            var mouseWorldPos = viewport.ScreenToTile(mousePt.X, mousePt.Y);
+            var centerWorldPos = viewport.ScreenToTile(gameViewRect.Width / 2, gameViewRect.Height / 2);
+            ImGui.Text($"Mouse @ {mouseWorldPos}");
+            ImGui.Text($"Center @ {centerWorldPos}");
             ImGui.End();
+
+            // Draw a small "cross-hair" at the center of the game view
+            var centerOfViewX = gameViewRect.X + gameViewRect.Width / 2f;
+            var centerOfViewY = gameViewRect.X + gameViewRect.Height / 2f;
+            Span<Line2d> lines = stackalloc Line2d[2]
+            {
+                new Line2d(
+                    new Vector2(centerOfViewX - 10, centerOfViewY),
+                    new Vector2(centerOfViewX + 10, centerOfViewY),
+                    PackedLinearColorA.White
+                ),
+                new Line2d(
+                    new Vector2(centerOfViewX, centerOfViewY - 10),
+                    new Vector2(centerOfViewX, centerOfViewY + 10),
+                    PackedLinearColorA.White
+                ),
+            };
+            Tig.ShapeRenderer2d.DrawLines(lines);
         }
     }
 }
